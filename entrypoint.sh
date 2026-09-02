bab1892a65b79b075e89ef788e9845b0cab3397c
Sofia Tseitlin <sofi.tseitlin@yandex.com>
2026-09-02 17:19:20 +0300

    Create cache table on startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 echo "→ Применяю миграции"
 python manage.py migrate --noinput
 
+echo "→ Проверяю таблицу кэша"
+python manage.py createcachetable
+
 if [ "$SKIP_COLLECTSTATIC" != "1" ]; then
   echo "→ Собираю статику"
   python manage.py collectstatic --noinput
